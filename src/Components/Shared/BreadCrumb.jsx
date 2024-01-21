caae9be2f7196562f824c7a29ffd1f4fcae6eb9a
bikashroy120 <bikashroydt@gmail.com>
2024-01-21 10:41:13 +0600

    add

--- src/Components/Shared/BreadCrumb.jsx
+++ src/Components/Shared/BreadCrumb.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const BreadCrumb = ({title,links}) => {
   return (
-    <div className=" lg:relative  lg:top-[-85px] left-0 mb-5 mt-3 lg:w-[400px] w-full lg:mt-0 lg:mb-[-50px] z-0">
+    <div className=" lg:relative  lg:top-[-75px] left-0 mb-5 mt-3 lg:w-[400px] w-full lg:mt-0 lg:mb-[-50px] z-0">
       <div>
         {links?.map((link,index)=>(
           <Link className="text-[#707EAE] text-[12px] font-[500]" key={index} to={link.url}>{link.title} {links.length - 1 > index ? "/" : ""} </Link>
