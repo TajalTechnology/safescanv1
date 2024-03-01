561cde18ae68b588e6220a90debde813389e6aba
bikashroy120 <bikashroydt@gmail.com>
2024-03-01 11:09:09 +0600

    note add

--- src/Components/pageComponents/Admins/Note.jsx
+++ src/Components/pageComponents/Admins/Note.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import NoteEdit from "./NoteEdit";
 import { Modal, Tooltip } from "antd";
 import { Icon } from "@iconify/react";
@@ -8,15 +8,37 @@ import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
 import DeleteModal from "../../Shared/modal/DeleteModal";
+import { useApproveUserMutation } from "../../../redux/features/admin/adminApi";
+import SuccessToast from "../../Shared/Toast/SuccessToast";
+import toast from "react-hot-toast";
+import ErrorToast from "../../Shared/Toast/ErrorToast";
+import { formattedDate } from "../../../helper/jwt";
 
 const Note = ({ row, refetch }) => {
   const [modalOPen, setModalOpen] = useState(false);
   const [itemIndex, setItemIndex] = useState(0);
   const [deletModal, setDeleteModal] = useState(false);
   const [editModal, setEditModal] = useState(false);
-
-
+  const [note, setNote] = useState("");
   const [activeIndex, setActiveIndex] = useState(0);
+
+  const [approveUser, { isLoading, isSuccess, error }] =
+    useApproveUserMutation();
+
+  useEffect(() => {
+    if (isSuccess) {
+      const message = "Add Note success";
+      toast.custom(<SuccessToast message={message} />);
+      refetch();
+      // setModalOpen(false);
+      setNote("")
+    }
+    if (error) {
+      toast.custom(
+        <ErrorToast message={error?.data.error || error?.data.message} />
+      );
+    }
+  }, [isSuccess, error]);
 
   console.log(activeIndex);
 
@@ -24,7 +46,21 @@ const Note = ({ row, refetch }) => {
     setActiveIndex(swiper.activeIndex);
   };
 
-  const NoteData = [0,1,2,3,4]
+  const onSubmit = async () => {
+    if (!note) {
+      toast.custom(
+        <ErrorToast message={"Please add note"} />
+      );
+    } else {
+      const body = {
+        username: row?.username,
+        note: note,
+        is_active: true,
+      };
+      const id = row?.userid;
+      await approveUser({ id, body });
+    }
+  };
 
   return (
     <>
@@ -34,7 +70,7 @@ const Note = ({ row, refetch }) => {
           onClick={() => setModalOpen(true)}
           className=" text-[14px]  font-normal text-info flex items-center w-full justify-between gap-1 "
         >
-          <span>{"03"}</span>
+          <span>{row?.notes?.length}</span>
           <Icon
             icon="material-symbols:edit-document-outline-rounded"
             className=" text-[20px]"
@@ -69,13 +105,17 @@ const Note = ({ row, refetch }) => {
               <textarea
                 name=""
                 id=""
+                value={note}
+                onChange={(e) => setNote(e.target.value)}
                 placeholder="Write Something..."
                 className="py-[15px] h-[95px] px-[14px]  text-dark-gray placeholder:text-[#A3AED0]  rounded-[10px] w-full text-sm font-medium outline-none  border-[1px] focus:border-primary"
               ></textarea>
             </div>
             <div className="w-[30%] flex items-center justify-center">
               <div className="flex items-center justify-center flex-col w-full gap-3">
-                <CustomButton className={" w-full"}>{"Add Note"}</CustomButton>
+                <CustomButton onClick={() => onSubmit()} className={" w-full"}>
+                  {isLoading ? "Loading..." : "Add Note"}
+                </CustomButton>
                 <button
                   type="button"
                   onClick={() => setModalOpen(false)}
@@ -98,52 +138,78 @@ const Note = ({ row, refetch }) => {
                 className="mySwiper"
                 onSlideChange={handleSlideChange}
               >
-                {NoteData?.map((card, index) => (
-                  <SwiperSlide
-                    className="flex items-center justify-center"
-                    key={index}
-                  >
-                    <div className="h-full flex items-center justify-center w-full">
-                      <div className="w-[85%] h-[80%] border rounded-[14px] p-5 border-[#C9D4EA]">
-                        <div className=" flex items-center justify-between">
-                          <h2 className=" text-[14px] font-bold text-dark-gray">
-                            24-02-2024
-                          </h2>
-                          <div className="flex items-center gap-2">
-                            <button onClick={()=>{setEditModal(true)}} className=" w-[30px] h-[30px] flex items-center justify-center rounded-lg border border-primary/40">
-                              <Icon
-                                icon="mingcute:pencil-line"
-                                className="text-[22px] hover:text-[#0070F0] text-primary"
-                              />
-                            </button>
-                            <button onClick={()=>{setItemIndex(index);setDeleteModal(true)}} className=" w-[30px] h-[30px] flex items-center justify-center rounded-lg border border-[#F40909]/40">
-                              <Icon
-                                icon="gg:trash-empty"
-                                className="text-[22px] hover:text-red-500 text-[#F40909]"
-                              />
-                            </button>
+                {row?.notes ? (
+                  <>
+                    {row?.notes?.map((card, index) => (
+                      <SwiperSlide
+                        className="flex items-center justify-center h-auto"
+                        key={index}
+                      >
+                        <div className="h-full flex items-center justify-center w-full">
+                          <div className="w-[85%] h-[100%] border rounded-[14px] p-5 border-[#C9D4EA]">
+                            <div className=" flex items-center justify-between">
+                              <h2 className=" text-[14px] font-bold text-dark-gray">
+                                  {formattedDate(card?.date)}
+                              </h2>
+                              <div className="flex items-center gap-2">
+                                <button
+                                  onClick={() => {
+                                    setEditModal(true);
+                                  }}
+                                  className=" w-[30px] h-[30px] flex items-center justify-center rounded-lg border border-primary/40"
+                                >
+                                  <Icon
+                                    icon="mingcute:pencil-line"
+                                    className="text-[22px] hover:text-[#0070F0] text-primary"
+                                  />
+                                </button>
+                                <button
+                                  onClick={() => {
+                                    setItemIndex(index);
+                                    setDeleteModal(true);
+                                  }}
+                                  className=" w-[30px] h-[30px] flex items-center justify-center rounded-lg border border-[#F40909]/40"
+                                >
+                                  <Icon
+                                    icon="gg:trash-empty"
+                                    className="text-[22px] hover:text-red-500 text-[#F40909]"
+                                  />
+                                </button>
+                              </div>
+                            </div>
+
+                            <div className=" mt-3">
+                              <p className=" text-[14px] font-normal text-[#485585]">
+                                {card?.note}
+                              </p>
+                            </div>
                           </div>
                         </div>
-
-                        <div className=" mt-3">
-                          <p className=" text-[14px] font-normal text-[#485585]">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit, sed do eiusmod tempor incididunt ut labore et
-                            dolore magna aliqua. Ut enim ad minim veniam, quis
-                            nostrud exercitation ullamco laboris nisi ut aliquip
-                            ex ea commodo consequat.
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </SwiperSlide>
-                ))}
+                      </SwiperSlide>
+                    ))}
+                  </>
+                ) : (
+                  <div className=" h-[100px] w-full flex items-center justify-center">
+                    <h2 className=" font-bold text-dark-gray text-[20px]">
+                      Please add a note
+                    </h2>
+                  </div>
+                )}
               </Swiper>
             </>
           </div>
           <div>
             <div className="w-full flex items-center justify-center">
-                <h2 className=" text-[#68769F] text-[16px]">{activeIndex+1}/{NoteData.length}</h2>
+              {row?.notes ? (
+                <>
+                  {" "}
+                  <h2 className=" text-[#68769F] text-[16px]">
+                    {activeIndex + 1}/{row?.notes?.length}
+                  </h2>
+                </>
+              ) : (
+                <></>
+              )}
             </div>
             <div className="flex mt-5 items-center justify-center flex-col w-full gap-3">
               <button
@@ -171,7 +237,9 @@ const Note = ({ row, refetch }) => {
         onDelete={() => {}}
         setModalOpen={setDeleteModal}
         title={"Delete Note!"}
-        title2={"Are you sure you want to delete this note? This action cannot be undone."}
+        title2={
+          "Are you sure you want to delete this note? This action cannot be undone."
+        }
       />
     </>
   );
